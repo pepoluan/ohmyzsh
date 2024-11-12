88895043a5dc6a1c46d4598c424ff8bd8348ec9c
Pandu POLUAN <pepoluan@gmail.com>
2024-11-12 17:15:32 +0700

    Use local var _subex instead of global var _SUBEX
    It's used only in one function anyways.
    This way we reduce pollution of global namespace.

--- plugins/gentoo/gentoo.plugin.zsh
+++ plugins/gentoo/gentoo.plugin.zsh
@@ -11,36 +11,35 @@
 
 # If in the future a new subexecuter is created, we only need to edit this array
 typeset _KNOWN_SUBEXES=( "doas" "sudo" )
-typeset _SUBEX
 
 function _SetupSubexecutor() {
+  local _subex
   local _i
   local _cmd
-  zstyle -s ':omz:plugins:gentoo' 'subexecutor' _SUBEX
-  if [[ "$_SUBEX" ]]; then
-    if command -v "$_SUBEX" > /dev/null; then
+  zstyle -s ':omz:plugins:gentoo' 'subexecutor' _subex
+  if [[ "$_subex" ]]; then
+    if command -v "$_subex" > /dev/null; then
       return 0
     fi
-    print "Cannot find subexecutor '${_SUBEX}'; please check your configuration!" >&2
+    print "Cannot find subexecutor '${_subex}'; please check your configuration!" >&2
     return 1
   fi
   for _i in "${_KNOWN_SUBEXES[@]}"; do
     if command -v "$_i" > /dev/null; then
-      _SUBEX="$_i"
+      _subex="$_i"
       break
     fi
   done
-  if [[ -z $_SUBEX ]]; then
+  if [[ -z $_subex ]]; then
     print "oh-my-zsh: cannot auto-detect subexecutor; please specify explicitly using 'zstyle :omz:plugins:gentoo subexecutor'." >&2
     return 1
   fi
-  zstyle ':omz:plugins:gentoo' 'subexecutor' "$_SUBEX"
+  zstyle ':omz:plugins:gentoo' 'subexecutor' "$_subex"
 }
 
 _SetupSubexecutor
 unfunction _SetupSubexecutor
 unset _KNOWN_SUBEXES
-unset _SUBEX
 
 function gsubex() {
   local _subex
