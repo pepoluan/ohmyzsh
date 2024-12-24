4b41d44f5bcc80c9227a733d9667aca3bfcae26a
Pandu POLUAN <pepoluan@gmail.com>
2024-12-24 12:59:07 +0700

    Add 'doas' to the termsupport stripping
    Because OpenDoas now is a viable alternative for sudo on personal
    systems.

--- lib/termsupport.zsh
+++ lib/termsupport.zsh
@@ -96,7 +96,7 @@ function omz_termsupport_preexec {
   fi
 
   # cmd name only, or if this is sudo or ssh, the next cmd
-  local CMD="${1[(wr)^(*=*|sudo|ssh|mosh|rake|-*)]:gs/%/%%}"
+  local CMD="${1[(wr)^(*=*|doas|sudo|ssh|mosh|rake|-*)]:gs/%/%%}"
   local LINE="${2:gs/%/%%}"
 
   title "$CMD" "%100>...>${LINE}%<<"
